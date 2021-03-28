12c3539f4cc2e791f51dbf86efa2e96c4c0fa642
Benjamin Priels <b.priels@wavenet.be>
2021-03-28 18:22:52 +0200

    Add project files.

---
+++ FirstGame.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstGame.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
